5a8a1abcf1e946fac5343b387a316af2af9b8b24
jberaldo <joaoangelio409@gmail.com>
2024-02-17 17:36:07 -0300

    Feat: Front controller design pattern

--- public/index.php
+++ public/index.php
@@ -1,8 +1,29 @@
 <?php
 
-require_once __DIR__.'/init.php';
+require_once '../vendor/autoload.php';
 
-$name = $request->query->get('name', 'World');
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
-$response->setContent(sprintf('Hello %s', htmlspecialchars($name, ENT_QUOTES, 'UTF-8')));
+
+$request = Request::createFromGlobals();
+$response = new Response();
+
+$map = [
+    '/hello' => __DIR__.'/../src/pages/hello.php',
+    '/bye' => __DIR__.'/../src/pages/bye.php'
+];
+
+$path = $request->getPathInfo();
+
+$path = $request->getPathInfo();
+if (isset($map[$path])) {
+    ob_start();
+    require $map[$path];
+    $response->setContent(ob_get_clean());
+} else {
+    $response->setStatusCode(404);
+    $response->setContent('Not Found');
+}
+
 $response->send();
