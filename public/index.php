fdce475f2e122c26fbd411429ed60ac19f1009bf
jberaldo <joaoangelo409@gmail.com>
2024-02-17 17:51:14 -0300

    Feat: cleaner front controller

--- public/index.php
+++ public/index.php
@@ -10,8 +10,8 @@ $request = Request::createFromGlobals();
 $response = new Response();
 
 $map = [
-    '/hello' => __DIR__.'/../src/pages/hello.php',
-    '/bye' => __DIR__.'/../src/pages/bye.php'
+    '/hello' => 'hello',
+    '/bye' => 'bye'
 ];
 
 $path = $request->getPathInfo();
@@ -19,11 +19,11 @@ $path = $request->getPathInfo();
 $path = $request->getPathInfo();
 if (isset($map[$path])) {
     ob_start();
-    require $map[$path];
+    extract($request->query->all(), EXTR_SKIP);
+    include sprintf(__DIR__.'/../src/pages/%s.php', $map[$path]);
     $response->setContent(ob_get_clean());
 } else {
-    $response->setStatusCode(404);
-    $response->setContent('Not Found');
+    $response = new Response('Not Found', 404);
 }
 
 $response->send();
